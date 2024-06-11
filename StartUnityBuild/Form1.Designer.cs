73c01bf0fe7478b50d3e35e99d9db09b9621d187
Jari Petays <jari.petays@iki.fi>
2024-06-11 10:36:07 +0300

    add File->Exit menu

--- StartUnityBuild/Form1.Designer.cs
+++ StartUnityBuild/Form1.Designer.cs
@@ -28,11 +28,64 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        menuStrip1 = new MenuStrip();
+        listView1 = new ListView();
+        fileToolStripMenuItem = new ToolStripMenuItem();
+        exitToolStripMenuItem = new ToolStripMenuItem();
+        menuStrip1.SuspendLayout();
+        SuspendLayout();
+        // 
+        // menuStrip1
+        // 
+        menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+        menuStrip1.Location = new Point(0, 0);
+        menuStrip1.Name = "menuStrip1";
+        menuStrip1.Size = new Size(1063, 24);
+        menuStrip1.TabIndex = 0;
+        menuStrip1.Text = "menuStrip1";
+        // 
+        // listView1
+        // 
+        listView1.Dock = DockStyle.Fill;
+        listView1.Location = new Point(0, 24);
+        listView1.Name = "listView1";
+        listView1.Size = new Size(1063, 539);
+        listView1.TabIndex = 1;
+        listView1.UseCompatibleStateImageBehavior = false;
+        // 
+        // fIleToolStripMenuItem
+        // 
+        fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+        fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+        fileToolStripMenuItem.Size = new Size(37, 20);
+        fileToolStripMenuItem.Text = "FIle";
+        // 
+        // exitToolStripMenuItem
+        // 
+        exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+        exitToolStripMenuItem.Size = new Size(180, 22);
+        exitToolStripMenuItem.Text = "Exit";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(1063, 563);
+        Controls.Add(listView1);
+        Controls.Add(menuStrip1);
+        MainMenuStrip = menuStrip1;
+        Name = "Form1";
+        Text = "Form1";
+        menuStrip1.ResumeLayout(false);
+        menuStrip1.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private MenuStrip menuStrip1;
+    private ToolStripMenuItem fileToolStripMenuItem;
+    private ToolStripMenuItem exitToolStripMenuItem;
+    private ListView listView1;
 }
--- StartUnityBuild/Form1.cs
+++ StartUnityBuild/Form1.cs
@@ -1,9 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace StartUnityBuild;
 
+[SuppressMessage("ReSharper", "LocalizableElement")]
 public partial class Form1 : Form
 {
     public Form1()
     {
         InitializeComponent();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        Text = "UNITY Build";
+        exitToolStripMenuItem.Click += (o, e) => Application.Exit();
     }
 }
