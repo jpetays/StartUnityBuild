d60e94b0fe2abc2ef29a1241e32b5728c3b0bf41
Jari Petays <jari.petays@iki.fi>
2024-06-12 09:41:34 +0300

    comment out menu disable/enable (thread issue)

--- StartUnityBuild/Form1.cs
+++ StartUnityBuild/Form1.cs
@@ -6,6 +6,8 @@ namespace StartUnityBuild;
 [SuppressMessage("ReSharper", "LocalizableElement")]
 public partial class Form1 : Form
 {
+    private static readonly char[] Separators = ['\r', '\n'];
+
     private static Form1 _instance = null!;
     private readonly string _currentDirectory;
     private string? _unityVersion;
@@ -28,18 +30,90 @@ public partial class Form1 : Form
         //listView1.GridLines = true;
         listView1.View = View.Details;
 
-        copyOutputToClipboardToolStripMenuItem.Click += (o, e) => CopyLines();
-        exitToolStripMenuItem.Click += (o, e) => Application.Exit();
-        gitStatusToolStripMenuItem.Click += (_, _) =>
+        copyOutputToClipboardToolStripMenuItem.Click += (_, _) => CopyLines();
+        exitToolStripMenuItem.Click += (_, _) => Application.Exit();
+        gitStatusToolStripMenuItem.Click += (_, _) => ExecuteMenuCommand(() =>
         {
             ClearLines();
             Commands.GitStatus(_currentDirectory);
-        };
-        startBuildToolStripMenuItem.Click += (_, _) =>
+        });
+        startBuildToolStripMenuItem.Click += (_, _) => ExecuteMenuCommand(() =>
         {
             ClearLines();
             Commands.UnityBuild(_currentDirectory, _buildTargets);
-        };
+        });
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        Text = "UNITY Build";
+        try
+        {
+            LoadEnvironment();
+        }
+        catch (Exception x)
+        {
+            AddLine($"Failed to LoadEnvironment");
+            AddLine("Error", $"{x.Message}");
+            if (x.StackTrace != null)
+            {
+                foreach (var line in x.StackTrace.Split(Separators, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    AddLine(line.Trim());
+                }
+            }
+        }
+        Text = $"UNITY {_unityVersion} Build : {_productName} ver {_productVersion} bundle {_bundleVersion}";
+        StartupCommand();
+    }
+
+    private void StartupCommand()
+    {
+        Thread.Yield();
+        ExecuteMenuCommand(() => Commands.GitStatus(_currentDirectory));
+    }
+
+    private void ExecuteMenuCommand(Action command)
+    {
+        try
+        {
+            DisableMenus();
+            command();
+        }
+        catch (Exception x)
+        {
+            AddLine("Error", $"{x.Message}");
+            foreach (ToolStripItem toolStripItem in menuStrip1.Items)
+            {
+                toolStripItem.Enabled = true;
+            }
+        }
+    }
+
+    private void DisableMenus()
+    {
+        return;
+        AddLine("-disable", $"menu count {menuStrip1.Items.Count}");
+        foreach (ToolStripItem toolStripItem in menuStrip1.Items)
+        {
+            toolStripItem.Enabled = false;
+        }
+    }
+
+    private void EnableMenus()
+    {
+        return;
+        if (InvokeRequired)
+        {
+            Invoke(() => EnableMenus);
+            return;
+        }
+        AddLine("-enable", $"menu count {menuStrip1.Items.Count}");
+        foreach (ToolStripItem toolStripItem in menuStrip1.Items)
+        {
+            toolStripItem.Enabled = true;
+        }
     }
 
     private void OnKeyDown(object sender, KeyEventArgs e)
@@ -50,31 +124,6 @@ public partial class Form1 : Form
         }
         CopyLines();
         e.SuppressKeyPress = true; // Stops other controls on the form receiving event.
-    }
-
-    protected override void OnLoad(EventArgs e)
-    {
-        base.OnLoad(e);
-        Text = "UNITY Build";
-        try
-        {
-            LoadEnvironment();
-            Commands.GitStatus(_currentDirectory);
-        }
-        catch (Exception x)
-        {
-            AddLine($"Failed to LoadEnvironment");
-            AddLine("Error", $"{x.Message}");
-            if (x.StackTrace != null)
-            {
-                foreach (var line in x.StackTrace.Split(new char[] { '\r', '\n' },
-                             StringSplitOptions.RemoveEmptyEntries))
-                {
-                    AddLine(line.Trim());
-                }
-            }
-        }
-        Text = $"UNITY {_unityVersion} Build : {_productName} ver {_productVersion} bundle {_bundleVersion}";
     }
 
     private void LoadEnvironment()
@@ -90,6 +139,7 @@ public partial class Form1 : Form
         AddLine("Builds", $"{string.Join(", ", _buildTargets)}");
     }
 
+    [SuppressMessage("ReSharper", "NullCoalescingConditionIsAlwaysNotNullAccordingToAPIContract")]
     public static void OutputListener(string prefix, string line)
     {
         if (string.IsNullOrEmpty(line))
@@ -102,6 +152,7 @@ public partial class Form1 : Form
     public static void ExitListener(string prefix, int exitCode)
     {
         AddLine($">{prefix}", $"exit: {exitCode}");
+        _instance.EnableMenus();
     }
 
     public static void AddLine(string prefix, string content)
@@ -109,22 +160,22 @@ public partial class Form1 : Form
         Color? color = prefix == "ERROR" ? Color.Red
             : prefix.StartsWith('.') ? Color.Gray
             : prefix.StartsWith('>') ? Color.Blue
-            : content.StartsWith('-') ? Color.Green
+            : content.StartsWith('-') ? Color.Magenta
+            : content.Contains("SUCCESSFULLY") ? Color.Green
             : null;
         AddLine($"{prefix,-12}: {content}", color);
     }
 
-    public static void ClearLines()
+    private void ClearLines()
     {
-        if (_instance.InvokeRequired)
+        if (InvokeRequired)
         {
-            _instance.Invoke(() => ClearLines);
+            Invoke(() => ClearLines);
             return;
         }
-        var listView = _instance.listView1;
-        listView.BeginUpdate();
-        listView.Items.Clear();
-        listView.EndUpdate();
+        listView1.BeginUpdate();
+        listView1.Items.Clear();
+        listView1.EndUpdate();
     }
 
     public static void AddLine(string line, Color? color = null)
