86b5c86b04301b87fcd9a658b51893b7109d14ac
Jari Petays <jari.petays@iki.fi>
2024-06-14 10:13:21 +0300

    create release script and zipped release

---
+++ _robocopy_release_folder.bat
@@ -0,0 +1,12 @@
+@echo off
+set SOURCE=.\StartUnityBuild\bin\Release\net8.0-windows
+set TARGET=..\..\StartUnityBuild\net8.0-windows
+set RELEASE=.\StartUnityBuild\release\StartUnityBuild.zip
+
+robocopy %SOURCE% %TARGET% *.* /S /E /V
+if exist %RELEASE% (
+	del /Q %RELEASE%
+)
+"C:\Program Files\7-Zip\7z.exe" a %RELEASE% %TARGET%
+echo.
+pause
