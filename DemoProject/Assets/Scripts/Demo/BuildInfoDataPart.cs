66ab94fcce9bdb320aef187d131e2072e86a4b58
Jari Petays <jari.petays@iki.fi>
2024-06-27 10:31:44 +0300

    add BuildInfoDataPart.cs

---
+++ DemoProject/Assets/Scripts/Demo/BuildInfoDataPart.cs
@@ -0,0 +1,10 @@
+namespace Demo
+{
+    internal static class MachineGeneratedBuildInfo
+    {
+        internal const string CompiledOnDateValue = "2024-06-25 12:26";
+        internal const int BundleVersionCodeValue = 3;
+        internal const int PatchValue = 2;
+        internal const bool IsMuteOtherAudioSourcesValue = false;
+    }
+}
