06d478fdb4635ade05305ceb1135e95ac553acbe
Jari Petays <jari.petays@iki.fi>
2024-07-02 08:59:40 +0300

    add PrgFrame (Managed plug-in DLL)

---
+++ PrgFrame/Info.cs
@@ -0,0 +1,10 @@
+﻿using Application = UnityEngine.Application;
+
+namespace PrgFrame
+{
+    public static class Info
+    {
+        public static string Version => "Prg Framework 1.0";
+        public static string UnityVersion => Application.unityVersion;
+    }
+}
