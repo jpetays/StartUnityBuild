30c82d84c95cff8e4b4c2fe7b92bc7c3dd799a5d
Jari Petays <jari.petays@iki.fi>
2024-07-02 11:50:29 +0300

    add PrgBuild.csproj

---
+++ PrgBuild/Build.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PrgBuild
+{
+    public static class Build
+    {
+        public static void BuildPlayer()
+        {
+            var unityVersion = Application.unityVersion;
+            Trace($"start BUILD in UNITY {unityVersion}");
+        }
+
+        private static void Trace(string message)
+        {
+            Debug.Log($"Prg_Build: {message}");
+        }
+    }
+}
