d594b2565ec0d98e6c5ecb3c12850b4e4bf1a252
Jari Petays <jari.petays@iki.fi>
2024-07-04 10:09:53 +0300

    create UNITY .meta file to log .txt assets

--- StartUnityBuild/BuildCommands.cs
+++ StartUnityBuild/BuildCommands.cs
@@ -1,3 +1,4 @@
+using System.Windows.Forms.VisualStyles;
 using NLog;
 
 namespace StartUnityBuild;
@@ -22,35 +23,84 @@ public static class BuildCommands
             var buildTarget = settings.BuildTargets[0];
             Form1.AddLine($">{outPrefix}", $"build {buildTarget}");
             var executable = settings.UnityExecutable;
-            var projectPath = Quoted(@".\");
-            var prevLogFile = Quoted(@$".\etc\_local_build_{buildTarget}.prev.log");
-            var tempLogFile = Quoted(@$".\etc\_local_build_{buildTarget}.temp.log");
-            var buildLogFile = Quoted(@$".\Assets\BuildReports\{buildTarget}.build.log.txt");
-            if (File.Exists(tempLogFile))
-            {
-                Logger.Trace($"File.Move({tempLogFile}, {prevLogFile}, overwrite: true);");
-                File.Move(tempLogFile, prevLogFile, overwrite: true);
-            }
-            if (File.Exists(buildLogFile))
-            {
-                Logger.Trace($"File.Delete({buildLogFile});");
-                File.Delete(buildLogFile);
-            }
+            var projectPath = settings.WorkingDirectory;
+            var prevLogFile = @$".\etc\_local_build_{buildTarget}.prev.log";
+            var buildLogFile = @$".\etc\_local_build_{buildTarget}.temp.log";
+            var outputLogFile = @$".\Assets\BuildReports\{buildTarget}.build.log.txt";
+            var outputLogMetafile = $"{outputLogFile}.meta";
+            PrepareFilesForBuild();
             var arguments =
-                $" -buildTarget {buildTarget} -projectPath {projectPath}" +
-                $" -logFile {tempLogFile}" +
+                $" -buildTarget {buildTarget} -projectPath {Quoted(projectPath)}" +
+                $" -logFile {Quoted(buildLogFile)}" +
                 $" -executeMethod PrgBuild.Build.BuildPlayer -quit -batchmode";
             Form1.AddLine($".{outPrefix}", $"executable: {executable}");
             Form1.AddLine($".{outPrefix}", $"arguments: {arguments}");
             var result = await RunCommand.Execute(outPrefix, executable, arguments,
                 settings.WorkingDirectory, null, Form1.OutputListener, Form1.ExitListener);
             Form1.AddLine($">{outPrefix}", $"return code {result}");
-            if (File.Exists(tempLogFile))
-            {
-                Logger.Trace($"File.Move({tempLogFile}, {buildLogFile}, overwrite: true);");
-                File.Move(tempLogFile, buildLogFile, overwrite: true);
-            }
+            HandleOutputFiles();
             finished();
+            return;
+
+            void PrepareFilesForBuild()
+            {
+                if (File.Exists(buildLogFile))
+                {
+                    Logger.Trace($"File.Move({buildLogFile}, {prevLogFile}, overwrite: true);");
+                    File.Move(buildLogFile, prevLogFile, overwrite: true);
+                }
+                if (File.Exists(outputLogFile))
+                {
+                    Logger.Trace($"File.Delete({outputLogFile});");
+                    File.Delete(outputLogFile);
+                    if (File.Exists(outputLogMetafile))
+                    {
+                        File.Delete(outputLogMetafile);
+                    }
+                }
+            }
+
+            void HandleOutputFiles()
+            {
+                if (!File.Exists(buildLogFile))
+                {
+                    return;
+                }
+                Logger.Trace($"File.Move({buildLogFile}, {outputLogFile}, overwrite: true);");
+                try
+                {
+                    File.Move(buildLogFile, outputLogFile, overwrite: true);
+                    if (!File.Exists(outputLogMetafile))
+                    {
+                        CreateUnityMetafile(outputLogMetafile);
+                    }
+                }
+                catch (Exception x)
+                {
+                    Form1.AddLine($">{outPrefix}", $"File.Move failed: {x.GetType().Name} {x.Message}");
+                }
+            }
         });
+    }
+
+    private static void CreateUnityMetafile(string metaFilename)
+    {
+        // Underscore '_' is placeholder for space in the raw string so trailing spaces are not stripped away!
+        const string metaFileContent = """
+                                       fileFormatVersion: 2
+                                       guid: 142774e6a1509ef46ba1d3ba55c8e8ce
+                                       TextScriptImporter:
+                                         externalObjects: {}
+                                         userData:_
+                                         assetBundleName:_
+                                         assetBundleVariant:_
+
+                                       """;
+        var ticks = DateTime.Now.Ticks.ToString()[^6..];
+        var randomGuid = $"{ticks}e6a1509ef46ba1d3ba55c8e8ce";
+        File.WriteAllText(metaFilename, metaFileContent
+            .Replace("142774e6a1509ef46ba1d3ba55c8e8ce", randomGuid)
+            .Replace('_', ' ')
+            .Replace("\r", ""));
     }
 }
