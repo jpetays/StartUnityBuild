6a9754c520c08b4b1e813304bf8f95e69c967ec8
Jari Petays <jari.petays@iki.fi>
2024-07-09 09:56:40 +0300

    GitStatus works

--- StartUnityBuild/Commands/GitCommands.cs
+++ StartUnityBuild/Commands/GitCommands.cs
@@ -10,19 +10,38 @@ public static class GitCommands
         const string outPrefix = "status";
         Task.Run(async () =>
         {
-            const string gitCommand = "status";
+            var gitCommand = "status --porcelain=v1";
             Form1.AddLine($">{outPrefix}", $"git {gitCommand}");
             await RunCommand.Execute(outPrefix, "git", gitCommand, workingDirectory,
-                null, GitOutputFilter, Form1.ExitListener);
+                null, GitStatusFilter, Form1.ExitListener);
+            gitCommand = "log --pretty=oneline origin/main..HEAD";
+            Form1.AddLine($">{outPrefix}", $"git {gitCommand}");
+            await RunCommand.Execute(outPrefix, "git", gitCommand, workingDirectory,
+                null, GitLogFilter, Form1.ExitListener);
             finished();
         });
     }
 
-    private static void GitOutputFilter(string prefix, string? line)
+    private static void GitStatusFilter(string prefix, string? line)
     {
-        if (line == null || line.StartsWith("  (use \"git"))
+        if (line == null)
         {
             return;
+        }
+        Form1.OutputListener(prefix, line.StartsWith("   ") ? line : $"-commit: {line}");
+    }
+
+    private static void GitLogFilter(string prefix, string? line)
+    {
+        if (line == null)
+        {
+            return;
+        }
+        var tokens = line.Split(' ');
+        if (tokens.Length >= 2 && tokens[0].Trim().Length == 40)
+        {
+            tokens[0] = "push:";
+            line = $"-{string.Join(' ', tokens)}";
         }
         Form1.OutputListener(prefix, line);
     }
