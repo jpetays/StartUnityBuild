7aba441abae1cd950d83b9c26eb9b8fa02d46cd7
Jari Petays <jari.petays@iki.fi>
2024-07-25 09:06:07 +0300

    auto update version 1.0.6

--- DemoProject/Assets/Scripts/Demo/BuildProperties.cs
+++ DemoProject/Assets/Scripts/Demo/BuildProperties.cs
@@ -2,8 +2,8 @@ namespace Demo
 {
     internal static class BuildProperties
     {
-        internal const string CompiledOnDate = "2024-07-25 08:31";
-        internal const int BundleVersionCode = 5;
+        internal const string CompiledOnDate = "2024-07-25 09:06";
+        internal const int BundleVersionCode = 6;
         internal const bool IsMuteOtherAudioSources = false;
     }
 }
